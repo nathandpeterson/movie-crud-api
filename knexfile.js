1a02791ca7d85388da2556215f0f5044f3329f47
Nathan Daniel Peterson <natperson@gmail.com>
2017-11-11 13:35:43 -0800

    change knexfile for deployment

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,6 @@ module.exports = {
   },
   production : {
     client: 'pg',
-    connection: 'process.env.DATABASE_URL'
+    connection: 'postgres://luvhxxsfbcvjlh:7f7f0c51965194fa4e00ae6421dbbb1c93bb4efeb7deb77ac3b26e96e5fd37c1@ec2-23-23-221-255.compute-1.amazonaws.com:5432/d99o2028r58v49'
   }
 }
